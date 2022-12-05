d65dc7156856158e43c3de8a2c245605a968a250
nitishkumar-shiv <108712673+nitishkumar-shiv@users.noreply.github.com>
2022-12-05 09:55:31 +0530

    image picjer added

--- src/Home/Spirit.js
+++ src/Home/Spirit.js
@@ -1,14 +1,134 @@
-import React from 'react';
-import {SafeAreaView, ScrollView, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Platform,
+  Alert,
+  Image,
+} from 'react-native';
+import * as ImagePicker from 'react-native-image-picker';
+import MultipleImagePicker from '@baronha/react-native-multiple-image-picker';
 
-export const Spirit = () => {
+import {storage} from '@react-native-firebase/storage';
+import * as Progress from 'react-native-progress';
+
+export const UploadScreen = () => {
+  const [image, setImage] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const [transferred, setTransferred] = useState(0);
+
+  const selectImage = async () => {
+    const options = {
+      doneTitle: 'NEXT->',
+      maxSelectedAssets: 30,
+      selectedColor: '#75FF33',
+
+      //storageOptions: {skipBackup: true, path: 'images'},
+    };
+    const response = await MultipleImagePicker.openPicker(options);
+    if (response.didCancel) {
+      console.log('User cancelled image picker');
+    } else if (response.error) {
+      console.log('ImagePicker Error: ', response.error);
+    } else if (response.customButton) {
+      console.log('User tapped custom button: ', response.customButton);
+    } else {
+      const source = {uri: response.assets[0].uri};
+      console.log(source);
+      setImage(source);
+    }
+  };
+
+  const uploadImage = async () => {
+    const {uri} = image;
+    const filename = uri.substring(uri.lastIndexOf('/') + 1);
+    const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri;
+    setUploading(true);
+    setTransferred(0);
+    const task = storage().ref(filename).putFile(uploadUri);
+    // set progress state
+    task.on('state_changed', snapshot => {
+      setTransferred(
+        Math.round(snapshot.bytesTransferred / snapshot.totalBytes) * 10000,
+      );
+    });
+    try {
+      await task;
+    } catch (e) {
+      console.error(e);
+    }
+    setUploading(false);
+    Alert.alert(
+      'Photo uploaded!',
+      'Your photo has been uploaded to Firebase Cloud Storage!',
+    );
+    setImage(null);
+  };
+
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View>
-          <Text>WELLCOME GUYS TO MY APP</Text>
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity style={styles.selectButton} onPress={selectImage}>
+        <Text style={styles.buttonText}>Pick an image</Text>
+      </TouchableOpacity>
+      <View style={styles.imageContainer}>
+        {image !== null ? (
+          <Image source={{uri: image.uri}} style={styles.imageBox} />
+        ) : null}
+        {uploading ? (
+          <View style={styles.progressBarContainer}>
+            <Progress.Bar progress={transferred} width={300} />
+          </View>
+        ) : (
+          <TouchableOpacity style={styles.uploadButton} onPress={uploadImage}>
+            <Text style={styles.buttonText}>Upload image</Text>
+          </TouchableOpacity>
+        )}
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#bbded6',
+  },
+  selectButton: {
+    borderRadius: 5,
+    width: 150,
+    height: 50,
+    backgroundColor: '#8ac6d1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  uploadButton: {
+    borderRadius: 5,
+    width: 150,
+    height: 50,
+    backgroundColor: '#ffb6b9',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  imageContainer: {
+    marginTop: 30,
+    marginBottom: 50,
+    alignItems: 'center',
+  },
+  progressBarContainer: {
+    marginTop: 20,
+  },
+  imageBox: {
+    width: 300,
+    height: 300,
+  },
+});
